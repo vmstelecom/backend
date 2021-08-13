af619f4783b23a0edc25043a017506facd4f6c08
vmstelecom <vmstelecom@gmail.com>
2021-08-13 08:56:36 -0300

    test getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,13 @@
-import {useState} from 'react';
+import Link from 'next/link';
 
 function Home(){
     return <div>
             <h2>Home</h2>
-            <Contador />
+            <Link href="/sobre">
+            <a>Outra</a>
+            </Link>
         </div>
 }
 
-function Contador(){
-    const [contador, setContador] = useState(1);
-
-    function adicionarContador(){
-        setContador(contador + 1);
-    }
-
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
-}
 
 export default Home
---
+++ pages/sobre.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+function Sobre(){
+    return <div>
+            <h2>Sobre</h2>
+            <Link href="/">
+            <a>Home</a>
+            </Link>
+        </div>
+}
+
+export default Sobre
