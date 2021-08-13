adcf19f5caf8f6996b8a0d11a2689df0913abbc7
vmstelecom <vmstelecom@gmail.com>
2021-08-13 09:23:05 -0300

    test getStaticProps

--- pages/tempo.js
+++ pages/tempo.js
@@ -5,26 +5,23 @@ function Tempo(props){
     return (
         <div>
             <div>{dynamicDateString} (dinâmico)</div>
-            <div>{props.staticDateString} (estático com delay)</div>
+            <div>{props.staticDateString} (estático)</div>
         </div>
     )
 }
 
 export async function getStaticProps(){
     console.log('getStaticProps');
-    console.log('delay');
 
-    await delay(5000);
     const staticDate = new Date();
     const staticDateString = staticDate.toGMTString();
 
     return {
         props: {
             staticDateString
-        }
+        },
+        revalidate: 1
     }
 }
-
-const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 export default Tempo;
